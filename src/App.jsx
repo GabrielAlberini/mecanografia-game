a7ed1f58efa339ee32ceb1c68c6cf485d9a29ba6
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-03-02 20:39:36 -0300

    fix: rethink the logic to make the text visible

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,65 @@
-import { useState, useEffect } from "react";
-import db from "./db/data.json";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const [dataLeng, setDataLeng] = useState([]);
-  const [randomText, setRandomText] = useState("");
+  const [index, setIndex] = useState(0);
+  const [word, setWord] = useState("hola como estas, muy bien y vos?");
+  const [partialWord, setPartialWord] = useState("");
+  const [error, setError] = useState(false); // Nuevo estado para gestionar errores
 
   useEffect(() => {
-    const randomIndex = Math.floor(Math.random() * dataLeng.length);
-    setRandomText(dataLeng[randomIndex]?.text);
-  }, [dataLeng]);
-
-  const selectText = (event) => {
-    const selectedLanguage = event.target.value;
-    const filteredData = db.filter(
-      (phrase) => phrase.language === selectedLanguage
-    );
-
-    setDataLeng(filteredData);
-
-    const randomIndex = Math.floor(Math.random() * filteredData.length);
-    setRandomText(filteredData[randomIndex]?.text);
-  };
-
-  const printText = (e) => {
-    if (e.key === randomText.charAt(0)) {
-      console.log("Es la letra.");
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", printText);
-
-    return () => {
-      document.removeEventListener("keydown", printText);
+    const handleKeyDown = (e) => {
+      if (e.key === word.charAt(index)) {
+        setError(false); // Restablecer el estado de error
+        setPartialWord(
+          (prevPartialWord) => prevPartialWord + word.charAt(index)
+        );
+        setIndex((prevIndex) => prevIndex + 1);
+      } else {
+        setError(true); // Establecer el estado de error si la letra es incorrecta
+      }
     };
-  }, [randomText]);
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    // Limpia el event listener en cada render
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [index, word, error]); // Agrega el estado de error a las dependencias del efecto
+
+  // Actualiza word fuera del efecto
+  useEffect(() => {
+    setWord(partialWord + word.slice(index));
+  }, [partialWord, index, word]);
+
+  console.log(partialWord, "string de palabra que se va haciendo");
+  console.log(index, "index");
+
+  const greenStyle = {
+    color: "green",
+  };
+
+  const redStyle = {
+    color: "red",
+  };
 
   return (
-    <>
-      <div>
-        <h1>Mecanografía App</h1>
-        <select onChange={selectText}>
-          <option value="es">ES</option>
-          <option value="fr">FR</option>
-          <option value="de">DE</option>
-          <option value="it">IT</option>
-          <option value="pt">PT</option>
-        </select>
-      </div>
-      <div onKeyDown={printText} tabIndex="0">
-        <p>{randomText}</p>
-      </div>
-    </>
+    <h1>
+      {word.split("").map((char, i) => (
+        <span
+          key={i}
+          style={
+            i < partialWord.length
+              ? greenStyle
+              : i === partialWord.length && error
+              ? redStyle
+              : {}
+          }
+        >
+          {char}
+        </span>
+      ))}
+    </h1>
   );
 };
 
